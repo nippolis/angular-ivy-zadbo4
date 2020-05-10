53aa52a6582a4913c307e7d590f2eb3d2da1ad96
nippolis <aieaaaae@gmail.com>
2020-05-10 19:57:30 +0200

    Initial commit

---
+++ src/app/topbarlol/topbarlol.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topbarlol',
+  templateUrl: './topbarlol.component.html',
+  styleUrls: ['./topbarlol.component.css']
+})
+export class TopbarlolComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
